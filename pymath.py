046c0a13ed157344c842e7bf790b13d6d54ee9a1
UtkarshSirohi <88072909+UtkarshSirohi@users.noreply.github.com>
2026-05-13 17:43:18 +0530

    Delete pymath.py

--- pymath.py
+++
@@ -1,7 +0,0 @@
-#integer slicing module
-def sliceint(num,decimals):
-    strnum=str(num)
-    findnum=strnum.find(".")
-    findnum1=findnum+(decimals+1)
-    printnum=strnum[:findnum1]
-    return float(printnum)
